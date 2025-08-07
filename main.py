3309eec3875d70cc86c42c06fb7f5328b8d62dd6
Rain <muther22@gmail.com>
2025-08-06 23:43:17 -0400

    moved xml parsing kanji finding functions to utility file

--- main.py
+++ main.py
@@ -1,21 +1,13 @@
 from sudachipy import Dictionary, SplitMode
-import xml.etree.ElementTree as ET
 import kenlm
 import heapq
 
 from util.jpUtil import hiragana_to_katakana
+from util.kanji import find_kanji_for_kana
 
 # todo: clean up this code, it is a mess
 
 tokenizer = Dictionary().create()
-
-JMDICT_PATH = "dict/JMdict_e.xml"
-tree = ET.parse(JMDICT_PATH)
-root = tree.getroot()
-
-KANJIDIC_PATH = "dict/kanjidic2.xml"
-kanjidic_tree = ET.parse(KANJIDIC_PATH)
-kanjidic_root = kanjidic_tree.getroot()
 
 posToCheck = [
     "普通名詞",
@@ -31,36 +23,6 @@ posToCheck = [
     "助動詞",
     "連体詞",
 ]
-
-
-def find_kanji_for_kana(reading):
-    results = set()
-    # results = find_kanji_for_kana_in_kanjidic(hiragana_to_katakana(reading))
-    for entry in root.findall("entry"):
-        readings = [
-            r.text for r_ele in entry.findall("r_ele") for r in r_ele.findall("reb")
-        ]
-        if reading in readings:
-            kanjis = [
-                k.text for k_ele in entry.findall("k_ele") for k in k_ele.findall("keb")
-            ]
-            if kanjis:
-                results.update(kanjis)
-    return results
-
-
-def find_kanji_for_kana_in_kanjidic(reading, reading_types=("ja_on", "ja_kun")):
-    results = set()
-    for char in kanjidic_root.findall("character"):
-        kanji = char.findtext("literal")
-        rm = char.find("reading_meaning")
-        if rm is not None:
-            for group in rm.findall("rmgroup"):
-                for rd in group.findall("reading"):
-                    rtype = rd.attrib.get("r_type")
-                    if rtype in reading_types and rd.text == reading:
-                        results.add(kanji)
-    return results
 
 
 def isVerbDictForm(verb):
---
+++ util/kanji.py
@@ -0,0 +1,39 @@
+import xml.etree.ElementTree as ET
+
+
+JMDICT_PATH = "dict/JMdict_e.xml"
+tree = ET.parse(JMDICT_PATH)
+root = tree.getroot()
+
+KANJIDIC_PATH = "dict/kanjidic2.xml"
+kanjidic_tree = ET.parse(KANJIDIC_PATH)
+kanjidic_root = kanjidic_tree.getroot()
+
+def find_kanji_for_kana(reading):
+    results = set()
+    # results = find_kanji_for_kana_in_kanjidic(hiragana_to_katakana(reading))
+    for entry in root.findall("entry"):
+        readings = [
+            r.text for r_ele in entry.findall("r_ele") for r in r_ele.findall("reb")
+        ]
+        if reading in readings:
+            kanjis = [
+                k.text for k_ele in entry.findall("k_ele") for k in k_ele.findall("keb")
+            ]
+            if kanjis:
+                results.update(kanjis)
+    return results
+
+
+def find_kanji_for_kana_in_kanjidic(reading, reading_types=("ja_on", "ja_kun")):
+    results = set()
+    for char in kanjidic_root.findall("character"):
+        kanji = char.findtext("literal")
+        rm = char.find("reading_meaning")
+        if rm is not None:
+            for group in rm.findall("rmgroup"):
+                for rd in group.findall("reading"):
+                    rtype = rd.attrib.get("r_type")
+                    if rtype in reading_types and rd.text == reading:
+                        results.add(kanji)
+    return results
